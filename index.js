d7958b5d76c66093aa955641a1b4969089d7c2c9
Kingsley <kadvani1@gmail.com>
2023-05-14 15:24:27 -0400

    update

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Embed from "./pages/embed.js";
+
+const App = () => {
+  return (
+    <div>
+      <Embed />
+    </div>
+  );
+};
+
+export default App;
---
+++ pages/embed.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Embed = () => {
+  return (
+    <div>
+      <iframe
+        src="https://allocations1.retool.com/apps/2023%20Admin%20Platform"
+        width="100%"
+        height="800px"
+      ></iframe>
+    </div>
+  );
+};
+
+export default Embed;
