2b393d7e538912fa4bc997e841292085e0ea815d
Kingsley <kadvani1@gmail.com>
2023-05-14 15:29:51 -0400

    admin panel

---
+++ admin/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+
+    <div>
+      <iframe
+        src="https://allocations1.retool.com/apps/2023%20Admin%20Platform"
+        width="100%"
+        height="800px"
+      ></iframe>
+    </div>
+  );
+};
