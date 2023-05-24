948b7eedaab081a1ba819622bb73b5989e72529c
Taylor Coffelt <taylorcoffelt@gmail.com>
2023-05-24 15:20:03 -0600

    Fix layout of ifram

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <iframe
         src="https://allocations1.retool.com/apps/4d203f40-f330-11ed-886b-4b9130e107d2/Admin%20Platform%20-%20Users"
         width="100%"
-        height="100vh"
+        height="100%"
       ></iframe>
     </div>
   );
